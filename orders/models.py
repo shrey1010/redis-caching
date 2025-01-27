1e24abaea7bd2f28bb05783c044dd17b1f37d736
Shrey Shukla <shreyshukla1010@gmail.com>
2025-01-28 00:27:34 +0530

    model setup

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    otp_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.product.name}"
